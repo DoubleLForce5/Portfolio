0fcbc537447faad1f211593c6b1fba0131872b9c
Liv <lopez.olivia17@outlook.com>
2021-07-15 12:46:00 -0500

    adjusted project cards

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,28 +1,29 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
 
-function ProjectCard (
-{  img, 
-  github, 
-  url, 
-  title, 
-  tech, 
-  description}) {
-    console.log(title)
-    return (
-    <section className='project-card'>
-      <div className='project-card-content'>
-      <div>
-        <img src={img} alt={title}/>
+function ProjectCard(props) {
+  console.log("props", props);
+  const { img, github, url, title, tech, description } = props.project;
+  return (
+    <section className="project-card">
+      <div className="project-img-container">
+        <img className="project-img" src={img} alt={title} />
       </div>
-      <div className='project-links'>Link</div>
-      <h3 className='project-title'>{title}</h3>
-      <ul className='project-topics'>topics</ul>
-      <p className='project-description'>description</p>
+      <div className="project-card-content">
+        <div className="project-links">
+          <a href={url} target="_blank" aria-label="link to deployed site">
+            <i class="bi bi-link-45deg"></i>
+          </a>
+          <a href={github} target="_blank" aria-label="link to github repo">
+            <i class="bi bi-github"></i>
+          </a>
+        </div>
+        <h3 className="project-title">{title}</h3>
+        <ul className="project-topics">{tech}</ul>
+        <p className="project-description">{description}</p>
       </div>
     </section>
-    )
-
+  );
 }
 
 export default ProjectCard;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,63 +1,52 @@
-import React from 'react';
-import ProjectCard from '../components/ProjectCard';
-import SectionTitle from '../components/SectionTitle';
+import React from "react";
+import ProjectCard from "../components/ProjectCard";
+import SectionTitle from "../components/SectionTitle";
 
-function Projects () {
+const projects = [
+  {
+    img: "assets/CMS-blog.png",
+    github: "https://github.com/DoubleLForce5/CMS_Tech_Blog",
+    url: "https://aqueous-gorge-22961.herokuapp.com/",
+    title: "CMS Blog",
+    tech: "Mysql, Node.js, Dotenv, Nodemon, Sequelize, Express, Handlebars, Javascript",
+    description:
+      "CMS-style blog site where individuals can publish posts, articles, thoughts, and opinions.",
+  },
+  {
+    img: "assets/CMS-blog.png",
+    github: "https://github.com/DoubleLForce5/CMS_Tech_Blog",
+    url: "https://aqueous-gorge-22961.herokuapp.com/",
+    title: "CMS Blog",
+    tech: "Mysql, Node.js, Dotenv, Nodemon, Sequelize, Express, Handlebars, Javascript",
+    description:
+      "CMS-style blog site where individuals can publish posts, articles, thoughts, and opinions.",
+  },
+  {
+    img: "assets/CMS-blog.png",
+    github: "https://github.com/DoubleLForce5/CMS_Tech_Blog",
+    url: "https://aqueous-gorge-22961.herokuapp.com/",
+    title: "CMS Blog",
+    tech: "Mysql, Node.js, Dotenv, Nodemon, Sequelize, Express, Handlebars, Javascript",
+    description:
+      "CMS-style blog site where individuals can publish posts, articles, thoughts, and opinions.",
+  },
+];
 
-  const projects = [
-    {
-      img: 'assets/CMS-blog.png',
-      github: 'https://github.com/DoubleLForce5/CMS_Tech_Blog',
-      url: 'https://aqueous-gorge-22961.herokuapp.com/',
-      title: 'CMS Blog',
-      tech: 'Mysql, Node.js, Dotenv, Nodemon, Sequelize, Express, Handlebars, Javascript',description: 'CMS-style blog site where individuals can publish posts, articles, thoughts, and opinions.'
-    },
-    {
-      img: 'assets/CMS-blog.png',
-      github: 'https://github.com/DoubleLForce5/CMS_Tech_Blog',
-      url: 'https://aqueous-gorge-22961.herokuapp.com/',
-      title: 'CMS Blog',
-      tech: 'Mysql, Node.js, Dotenv, Nodemon, Sequelize, Express, Handlebars, Javascript',description: 'CMS-style blog site where individuals can publish posts, articles, thoughts, and opinions.'
-    },
-    {
-      img: 'assets/CMS-blog.png',
-      github: 'https://github.com/DoubleLForce5/CMS_Tech_Blog',
-      url: 'https://aqueous-gorge-22961.herokuapp.com/',
-      title: 'CMS Blog',
-      tech: 'Mysql, Node.js, Dotenv, Nodemon, Sequelize, Express, Handlebars, Javascript',description: 'CMS-style blog site where individuals can publish posts, articles, thoughts, and opinions.'
-    }
-  ]
-
-  function renderProjectCards ({
-    img, 
-    github, 
-    url, 
-    title, 
-    tech, 
-    description}) {
-    return (
-      <ProjectCard
-        img={img}
-        github={github}
-        url={url}
-        title={title}
-        tech={tech}
-        description={description}
-      />
-    )
+function Projects() {
+  function renderProjectCards(project) {
+    console.log("from project page", project);
+    return <ProjectCard project={project} />;
   }
 
   return (
     <>
-    <SectionTitle>
-    <em>Projects</em>
-    </SectionTitle>
-      
-    <ProjectCard>
-    {projects.map((project) => renderProjectCards(project))}
-    </ProjectCard>
+      <SectionTitle>
+        <em>Projects</em>
+      </SectionTitle>
+
+      {projects.map((project) => renderProjectCards(project))}
     </>
-  )
+  );
 }
 
 export default Projects;
