0d8547f6dfe4aaffda3168515d9136e119a7ee0c
Liv <lopez.olivia17@outlook.com>
2021-09-16 17:21:32 -0500

    changed sentence in contact me section

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -11,37 +11,47 @@ function Contact() {
 
       <ContactSection>
         <p className="contact-text">
-          Something peak your interest? Get in touch with me and let's talk.
+          See something interesting, or wanna have a quick chat?
         </p>
-        <p className='email-tag'><a
+        <p className="email-tag">
+          <a
             className="email"
             href="mailto:lopez.olivia17@outlook.com"
             target="_blank"
             aria-label="link to my email"
             rel="noopener noreferrer"
-          >lopez.olivia17@outlook.com</a></p>
+          >
+            lopez.olivia17@outlook.com
+          </a>
+        </p>
         <div className="contact-links">
-         <a
+          <a
             className="linkedin-icon contact-icon"
             href="https://www.linkedin.com/in/olivialopez17/"
             target="_blank"
             aria-label="link to my linkedIn"
             rel="noopener noreferrer"
-          ><i class="bi bi-linkedin"></i></a>
+          >
+            <i class="bi bi-linkedin"></i>
+          </a>
           <a
             className="github-icon contact-icon"
             href="https://github.com/DoubleLForce5"
             target="_blank"
             aria-label="link to my github"
             rel="noopener noreferrer"
-          ><i class="bi bi-github"></i></a>
+          >
+            <i class="bi bi-github"></i>
+          </a>
           <a
             className="resume-icon contact-icon"
             href="assets/olivia-lopez-resume.pdf"
             target="_blank"
             aria-label="link to my resume"
             rel="noopener noreferrer"
-          ><i class="bi bi-file-earmark-person-fill"></i></a>
+          >
+            <i class="bi bi-file-earmark-person-fill"></i>
+          </a>
         </div>
       </ContactSection>
     </>
