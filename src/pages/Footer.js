ae9acb953754fa979c30b04b76c23a439368e7ec
Liv <lopez.olivia17@outlook.com>
2021-09-16 01:59:29 -0500

    modified footer style.css and page

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -6,13 +6,13 @@ function Footer() {
     <>
       <FooterSection>
         <div className="footer-content">
-        <p className="footer-text">
-          Site made with{" "}
-          <span role="img" aria-label="red heart emoji">
-            &#9825;
-          </span>{" "}
-          by Olivia
-        </p>
+          <p className="footer-text-one">
+            Made with{" "}
+            <span role="img" aria-label="red heart emoji">
+              &#9825;
+            </span>{" "} by Olivia Lopez
+          </p>
+          <p className="footer-text-two">Built with HTML, CSS, JavaScript, & React</p>
         </div>
       </FooterSection>
     </>
