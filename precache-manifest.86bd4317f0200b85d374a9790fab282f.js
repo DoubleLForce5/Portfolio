6f72470a43cb83b229eea1871507643dcc0b20de
Liv <lopez.olivia17@outlook.com>
2021-09-16 11:35:34 -0500

    Updates

---
+++ precache-manifest.86bd4317f0200b85d374a9790fab282f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "41dff4693411e96f9ec3",
+    "url": "/Portfolio/static/css/main.b8f5642e.chunk.css"
+  },
+  {
+    "revision": "41dff4693411e96f9ec3",
+    "url": "/Portfolio/static/js/main.0967f4af.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "31b7b688fd17f824fa50",
+    "url": "/Portfolio/static/js/2.c52eb6ea.chunk.js"
+  },
+  {
+    "revision": "56a72ffe3739897d30e509578c69d53b",
+    "url": "/Portfolio/index.html"
+  }
+];
